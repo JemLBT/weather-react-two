d1cd88819d9bace664c40510c3706c062590b6d1
JemLBT <adesignjem@gmail.com>
2025-03-28 16:23:40 +0000

    setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from "react";
+import Search from "./Search";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Search />
+      <h2></h2>
+    </div>
+  );
+}
---
+++ src/Search.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function Search() {
+  let [city, setCity] = useState("");
+  let [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setMessage(`The temperature in ${city} is 10°C`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Enter a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      <h2>{message}</h2>
+    </div>
+  );
+}
